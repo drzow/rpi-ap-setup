665f2ddc1ff1b61c500613be378f4cd3278cfaef
Zow" Terry Brugger, PhD <zow@acm.org>
2019-11-21 23:00:16 -0600

    Cleanup; removed WPA conf; working on wlan1 conf; added forwarding.

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ Based on https://gist.github.com/Lewiscowles1986/fecd4de0b45b2029c390
 
 Changes from the original:
 - Expanded DHCP space
-- Retain Wireless Networking config for secondary WiFi interfaces (wlan1) (TODO)
-- Set up a bridged connection from the AP on wlan0 to wlan1 -- assuming wlan1 has Internet access,
-  this will provide all AP users Internet access (TODO)
+- Retain Wireless Networking config for secondary WiFi interfaces (wlan1) (in progress)
+- Set up a forwarded connection from the AP on wlan0 to wlan1 -- assuming wlan1 has Internet access,
+  this will provide all AP users Internet access (in progress)
 - Removed WPA configuration so that the Access Point is wide open -- NOTE: This is not what most
-  users will want or need, but is necessary in limited settings (a public meeting space, in this case) (TODO)
+  users will want or need, but is necessary in limited settings (a public meeting space, in this case)
--- rpi-ap-setup.sh
+++ rpi-ap-setup.sh
@@ -8,18 +8,17 @@ if [ "$EUID" -ne 0 ]
 	exit
 fi
 
-if [[ $# -lt 1 ]]; 
+if [[ $# -gt 1 ]]; 
 	then echo "You need to pass a password!"
 	echo "Usage:"
-	echo "sudo $0 yourChosenPassword [apName]"
+	echo "sudo $0 [apName]"
 	exit
 fi
 
-APPASS="$1"
 APSSID="rPi3"
 
 if [[ $# -eq 2 ]]; then
-	APSSID=$2
+	APSSID=$1
 fi
 
 apt-get remove --purge hostapd -yqq
@@ -37,32 +36,14 @@ interface=wlan0
 hw_mode=g
 channel=10
 auth_algs=1
-wpa=2
-wpa_key_mgmt=WPA-PSK
-wpa_pairwise=CCMP
 rsn_pairwise=CCMP
-wpa_passphrase=$APPASS
 ssid=$APSSID
 ieee80211n=1
 wmm_enabled=1
 ht_capab=[HT40][SHORT-GI-20][DSSS_CCK-40]
 EOF
 
-sed -i -- 's/allow-hotplug wlan0//g' /etc/network/interfaces
-sed -i -- 's/iface wlan0 inet manual//g' /etc/network/interfaces
-sed -i -- 's/    wpa-conf \/etc\/wpa_supplicant\/wpa_supplicant.conf//g' /etc/network/interfaces
 sed -i -- 's/#DAEMON_CONF=""/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/g' /etc/default/hostapd
-
-cat >> /etc/network/interfaces <<EOF
-# Added by rPi Access Point Setup
-allow-hotplug wlan0
-iface wlan0 inet static
-	address 192.168.4.1
-	netmask 255.255.255.0
-	network 192.168.4.0
-	broadcast 192.168.4.255
-
-EOF
 
 echo "denyinterfaces wlan0" >> /etc/dhcpcd.conf
 
@@ -70,7 +51,13 @@ systemctl unmask hostapd
 systemctl enable hostapd
 systemctl enable dnsmasq
 
-sudo service hostapd start
-sudo service dnsmasq start
+service hostapd start
+service dnsmasq start
+
+# Bridge the interfaces
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+iptables -t nat -A POSTROUTING -o wlan1 -j MASQUERADE
+iptables-save > /etc/iptables.ipv4.nat
+echo "iptables-restore < /etc/iptables.ipv4.nat" >> /etc/rc.local
 
 echo "All done! Please reboot"
